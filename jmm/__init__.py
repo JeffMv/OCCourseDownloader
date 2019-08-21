0a6fb5f83987b3ad1043858122764039cecf849c
Jeffrey Mvutu <jeffrey.mvutu@gmail.com>
2019-08-21 18:36:51 +0200

    added dependency as a local module

---
+++ jmm/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+
+__author__ = "Jeffrey Mvutu Mabilama (jeffrey.mvutu@gmail.com)"
+__version__ = "0.1.2.7.10"
+__copyright__ = "Copyright (c) 2017-2019 Jeffrey Mvutu Mabilama"
+__license__ = "All rights reserved"
+
